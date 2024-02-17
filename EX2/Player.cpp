9e62e78e8f894949c550442721136a0754eeb273
Zohar <azohar@campus.technion.ac.il>
2024-02-17 16:26:45 +0200

    Initial commit

---
+++ EX2/Player.cpp
@@ -0,0 +1,295 @@
+#include <iostream>
+#include <cstring>
+class Player
+{
+private:
+    char* name;
+    int level;
+    int force;
+    int hp;
+    int maxHp;
+    int coins;
+    const char* DEFAULT_NAME = "player";
+    const int DEFAULT_LEVEL = 1;
+    const int DEFAULT_FORCE = 5;
+    const int DEFAULT_HP = 100;
+    const int DEFAULT_MAXHP = 100;
+    const int DEFAULT_COINS = 0;
+    const int MAXIMUM_LEVEL = 10;
+    const int MINIMUM_LEVEL = 1;
+    const int MINIMUM_FORCE = 0;
+    const int MINIMUM_HP = 0;
+    const int MINIMUM_MAXHP = 1;
+    const int MINIMUM_COINS = 0;
+    //test someting
+public:
+    Player(char* name_, int level, int force, int hp, int maxHp, int coins);
+    Player(char* name_, int force, int maxHp);
+    Player(char* name_, int maxHp);
+    Player(char* name_);
+    Player();
+    Player(const Player& other); // Copy constructor 
+    ~Player();
+    void printInfo();
+    Player& operator=(const Player& other);
+    void levelUp();
+    int getLevel();
+    void buff(int attack);
+    void heal(int potion);
+    void damage(int dmg);
+    void isKnockOut();
+    void addCoins(int money);
+    bool pay(int transaction);
+    int getAttackStrength();
+
+private:
+    bool inputValidation(char*& name, int& level, int& force, int& hp, int& maxHp, int& coins);
+    void printPlayerInfo(const char* name, int level, int force, int hp, int coins);
+
+};
+
+Player::Player(char* name_, int level, int force, int hp, int maxHp, int coins)
+{
+    char* name = new char[strlen(name_) + 1];
+    strcpy(name, name_);
+    inputValidation(name, level, force, hp, maxHp, coins);
+    this->name = name;
+    this->level = level;
+    this->force = force;
+    this->hp =hp;
+    this->maxHp = maxHp;
+    this->coins = coins;
+}
+
+Player::Player(char* name_, int force, int maxHp){
+
+    int hp = maxHp;
+    int coins = DEFAULT_COINS;
+    int level = DEFAULT_LEVEL;
+    char* name = new char[strlen(name_) + 1];
+    strcpy(name, name_);
+    inputValidation(name, level, force, hp, maxHp, coins);
+    this->name = name;
+    this->level = level;
+    this->force = force;
+    this->hp = hp;
+    this->maxHp = maxHp;
+    this->coins = coins;
+
+
+}
+
+Player::Player(char* name_, int maxHp){
+
+    int hp = maxHp;
+    int coins = DEFAULT_COINS;
+    int level = DEFAULT_LEVEL;
+    int force = DEFAULT_FORCE;
+    char* name = new char[strlen(name_) + 1];
+    strcpy(name, name_);
+    inputValidation(name, level, force, hp, maxHp, coins);
+    this->name = name;
+    this->level = level;
+    this->force = force;
+    this->hp = hp;
+    this->maxHp = maxHp;
+    this->coins = coins;
+
+
+}
+
+Player::Player(char* name_){
+
+    int hp = maxHp;
+    int coins = DEFAULT_COINS;
+    int level = DEFAULT_LEVEL;
+    int force = DEFAULT_FORCE;
+    int maxHp = DEFAULT_MAXHP;
+    char* name = new char[strlen(name_) + 1];
+    strcpy(name, name_);
+    inputValidation(name, level, force, hp, maxHp, coins);
+    this->name = name;
+    this->level = level;
+    this->force = force;
+    this->hp = hp;
+    this->maxHp = maxHp;
+    this->coins = coins;
+
+}
+
+Player::Player(){
+    int maxHp = DEFAULT_MAXHP;
+    int hp = maxHp;
+    int coins = DEFAULT_COINS;
+    int level = DEFAULT_LEVEL;
+    int force = DEFAULT_FORCE;
+    char* name = new char[strlen(DEFAULT_NAME) + 1];
+    strcpy(name, DEFAULT_NAME);
+    inputValidation(name, level, force, hp, maxHp, coins);
+    this->name = name;
+    this->level = level;
+    this->force = force;
+    this->hp = hp;
+    this->maxHp = maxHp;
+    this->coins = coins;
+
+}
+
+Player::Player(const Player& other){
+
+    this->level = other.level;
+    this->force = other.force;
+    this->hp = other.hp;
+    this->maxHp = other.maxHp;
+    this->coins = other.coins;
+    delete[] this->name; // named should be always not null
+    this->name = new char[strlen(other.name) + 1];
+    strcpy(name,other.name);
+}
+
+
+Player::~Player()
+{
+    delete[] name;
+}
+
+
+Player& Player::operator=(const Player& other) {
+    if(this!=&other){
+        delete[] name;
+        level = other.level;
+        force = other.force;
+        hp = other.hp;
+        maxHp = other.maxHp;
+        coins = other.coins;
+        name = new char[strlen(other.name) + 1];
+        strcpy(name, other.name);
+
+    }
+    return *this;
+}
+
+
+void Player::printPlayerInfo(const char* name, int level, int force, int hp, int coins){
+
+    std::cout << "Player Details:\n" << std::endl;
+    std::cout << "Name: " << name << "\n"<< std::endl;
+    std::cout << "Level: " << level << "\n"<< std::endl;
+    std::cout << "Force: " << force << "\n"<< std::endl;
+    std::cout << "HP: " << hp << "\n"<< std::endl;
+    std::cout << "Coins: " << coins << "\n"<< std::endl;
+    std::cout << "------------------------\n" << std::endl;
+
+
+}
+
+void Player::printInfo(){
+    printPlayerInfo(this->name,this->level,this->force,this->hp,this->coins);
+}
+
+void Player::levelUp(){
+    if(this->level<MAXIMUM_LEVEL){
+        this->level++;
+    }
+    else{
+        std::cout << "Player already reached max level: " <<std::endl;
+    }
+}
+
+int Player::getLevel(){
+    return this->level;
+}
+
+void Player::buff(int attack){
+    this->force+=attack;
+}
+
+void Player::heal(int potion){
+    int temp = this->hp+potion;
+    this->hp = temp > this->maxHp ? this->maxHp : temp;
+}
+
+void Player::damage(int attack){
+    int temp = this->hp-attack;
+    this->hp = temp < MINIMUM_HP ? MINIMUM_HP : temp;
+}
+
+void Player::isKnockOut(){
+    if(this->hp == MINIMUM_HP){  // shoudnt be sincero that hp < MINIMUM_HP
+        std::cout << "Player is knocked out";
+    }
+    else{
+        std::cout << "Player is not knocked out";
+    }
+}
+
+void Player::addCoins(int money){
+    this->coins+=money;
+}
+
+bool Player::pay(int transaction){
+    int temp = this->coins-transaction;
+    if (temp < MINIMUM_COINS)
+    {
+        std::cout << "Not enough coins";
+        return false;
+    }
+    this->coins = temp;
+    std::cout << "Transaction made successfully" <<std::endl;
+    return true;
+}
+
+
+int Player::getAttackStrength(){
+    return this->force+this->level;
+}
+
+bool Player::inputValidation(char*& name, int& level, int& force, int& hp, int& maxHp, int& coins)
+{
+    bool unchanged = true;
+    if (level < MINIMUM_LEVEL || level > MAXIMUM_LEVEL)
+    {
+        level = DEFAULT_LEVEL;
+        unchanged = false;
+        std::cout << "The value of 'level' is invalid value changed to default setting:  " << DEFAULT_LEVEL << std::endl;
+    }
+
+       if (force < MINIMUM_FORCE)
+    {
+        force = DEFAULT_FORCE;
+        unchanged = false;
+        std::cout << "The value of 'force' is invalid, value changed to default setting:  " << DEFAULT_FORCE << std::endl;
+
+    }
+
+        if (maxHp < MINIMUM_MAXHP)
+    {
+        maxHp = DEFAULT_MAXHP;
+        unchanged = false;
+        std::cout << "The value of 'maxHp' is invalid, value changed to default setting:  " << DEFAULT_MAXHP << std::endl;
+    }
+
+        if (hp < MINIMUM_HP || hp > maxHp)
+    {
+        hp = maxHp;
+        unchanged = false;
+        std::cout << "The value of 'hp' is invalid, value changed to maxHp value :  " << maxHp << std::endl;
+    }
+        if (coins < MINIMUM_COINS)
+    {
+        coins = DEFAULT_COINS;
+        unchanged = false;
+        std::cout << "The value of 'coins' is invalid, value changed to default setting:  " << DEFAULT_COINS << std::endl;
+    }
+    char* temp = name;
+    bool nameIsValid =true;
+    while(temp != '\0' && nameIsValid){
+        if(*temp < 'a' || *temp > 'z' ){
+            unchanged = false;
+            std::cout << "The value of 'name' is invalid, value changed to default setting:  " << DEFAULT_NAME << std::endl;
+            nameIsValid =false;
+        }
+        temp++;
+    }
+    return unchanged;
+}
---
+++ EX2/Player.h
@@ -0,0 +1,166 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+#include <cstring>
+
+class Player {
+private:
+    char* name; /**< The name of the player */
+    int level; /**< The level of the player */
+    int force; /**< The force of the player */
+    int hp; /**< The current hit points of the player */
+    int maxHp; /**< The maximum hit points of the player */
+    int coins; /**< The number of coins the player has */
+
+    static const char* DEFAULT_NAME; /**< The default name for a player */
+    static const int DEFAULT_LEVEL; /**< The default level for a player */
+    static const int DEFAULT_FORCE; /**< The default force for a player */
+    static const int DEFAULT_HP; /**< The default hit points for a player */
+    static const int DEFAULT_MAXHP; /**< The default maximum hit points for a player */
+    static const int DEFAULT_COINS; /**< The default number of coins for a player */
+    static const int MAXIMUM_LEVEL; /**< The maximum level a player can reach */
+    static const int MINIMUM_LEVEL; /**< The minimum level a player can have */
+    static const int MINIMUM_FORCE; /**< The minimum force a player can have */
+    static const int MINIMUM_HP; /**< The minimum hit points a player can have */
+    static const int MINIMUM_MAXHP; /**< The minimum maximum hit points a player can have */
+    static const int MINIMUM_COINS; /**< The minimum number of coins a player can have */
+
+    /**
+     * @brief Validates the input values for the player attributes.
+     * @param name The name of the player.
+     * @param level The level of the player.
+     * @param force The force of the player.
+     * @param hp The hit points of the player.
+     * @param maxHp The maximum hit points of the player.
+     * @param coins The number of coins the player has.
+     * @return True if the input values are valid, false otherwise.
+     */
+    bool inputValidation(char*& name, int& level, int& force, int& hp, int& maxHp, int& coins);
+
+    /**
+     * @brief Prints the player's information.
+     * @param name The name of the player.
+     * @param level The level of the player.
+     * @param force The force of the player.
+     * @param hp The hit points of the player.
+     * @param coins The number of coins the player has.
+     */
+    void printPlayerInfo(const char* name, int level, int force, int hp, int coins);
+
+public:
+    /**
+     * @brief Default constructor. Initializes player with default values.
+     */
+    Player();
+
+    /**
+     * @brief Constructor with parameters.
+     * @param name_ The name of the player.
+     * @param level The level of the player.
+     * @param force The force of the player.
+     * @param hp The hit points of the player.
+     * @param maxHp The maximum hit points of the player.
+     * @param coins The number of coins the player has.
+     */
+    Player(char* name_, int level, int force, int hp, int maxHp, int coins);
+
+    /**
+     * @brief Constructor with parameters.
+     * @param name_ The name of the player.
+     * @param force The force of the player.
+     * @param maxHp The maximum hit points of the player.
+     */
+    Player(char* name_, int force, int maxHp);
+
+    /**
+     * @brief Constructor with parameters.
+     * @param name_ The name of the player.
+     * @param maxHp The maximum hit points of the player.
+     */
+    Player(char* name_, int maxHp);
+
+    /**
+     * @brief Constructor with parameters.
+     * @param name_ The name of the player.
+     */
+    Player(char* name_);
+
+    /**
+     * @brief Copy constructor.
+     * @param other The other player to copy from.
+     */
+    Player(const Player& other);
+
+    /**
+     * @brief Destructor.
+     */
+    ~Player();
+
+    /**
+     * @brief Assignment operator.
+     * @param other The other player to assign from.
+     * @return Reference to the assigned player.
+     */
+    Player& operator=(const Player& other);
+
+    /**
+     * @brief Prints the player's information.
+     */
+    void printInfo();
+
+    /**
+     * @brief Increases the player's level by 1, if possible.
+     */
+    void levelUp();
+
+    /**
+     * @brief Gets the level of the player.
+     * @return The level of the player.
+     */
+    int getLevel();
+
+    /**
+     * @brief Increases the player's force.
+     * @param attack The amount of force to increase.
+     */
+    void buff(int attack);
+
+    /**
+     * @brief Increases the player's hit points by a given amount, up to the maximum hit points.
+     * @param potion The amount of hit points to increase.
+     */
+    void heal(int potion);
+
+    /**
+     * @brief Reduces the player's hit points by a given amount.
+     * @param dmg The amount of hit points to reduce.
+     */
+    void damage(int dmg);
+
+    /**
+     * @brief Checks if the player is knocked out (has 0 hit points).
+     */
+    void isKnockOut();
+
+    /**
+     * @brief Adds coins to the player's inventory.
+     * @param money The amount of coins to add.
+     */
+    void addCoins(int money);
+
+    /**
+     * @brief Pays a transaction using coins from the player's inventory.
+     * @param transaction The amount of coins to pay.
+     * @return True if the transaction was successful, false if the player does not have enough coins.
+     */
+    bool pay(int transaction);
+
+    /**
+     * @brief Calculates the player's attack strength.
+     * @return The player's attack strength.
+     */
+    int getAttackStrength();
+};
+
+#endif // PLAYER_H
